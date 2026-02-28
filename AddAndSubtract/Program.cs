b67dece9d0cab424890ea7297ffedecc69e87901
DESKTOP-G32PLXT\Test <melisacausheva@gmail.com>
2026-03-01 00:49:08 +0200

    AddAndSubtract

---
+++ AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sum = Sum(firstNumber, secondNumber);
+            int diff = Subtract(sum, thirdNumber);
+
+            Console.WriteLine(diff);
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
