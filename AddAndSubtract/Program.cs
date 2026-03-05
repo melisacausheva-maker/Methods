ba339f6ae1c274101ace6b292e9383e7611101aa
Melissa Chausheva <melisacausheva@gmail.com>
2026-03-05 20:16:47 +0200

    Add user input for three integers
    Read three integers from user input and calculate the difference.

--- AddAndSubtract/Program.cs
+++ AddAndSubtract/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-        
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            
             int sum = Sum(firstNumber, secondNumber);
             int diff = Subtract(sum, thirdNumber);
 
